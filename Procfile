3ec0234d68990e0d4c88054c5085dadf2103bb7f
asaffe <asaffe.m@gmail.com>
2020-10-03 02:35:04 -0300

    deploy no heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn djHerokuPipeline.wsgi --log-file -
--- djHerokuPipeline/settings.py
+++ djHerokuPipeline/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '0gzg13x6x3k(ckqz5k5cr(tul%c5lf)7&nm#!-z+d^*4z)l=qc'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -118,4 +118,5 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/static/'  # desenvolvimento
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # produção
